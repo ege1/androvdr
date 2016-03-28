b3bddc2281e7a21019fc8c6670e39754cd21b73e
Erik Geiger <Erik@geiger-online.ch>
2016-03-29 00:15:49 +0200

    Added files generated by android studio, created vpi_attrs.xml to mitigate compile errors and changed AndroApplication.java to be able to use the app again, because most probably it didn't work because auf acra debugger that doesn't allow usage of google docs any more

--- src/de/androvdr/AndroApplication.java
+++ src/de/androvdr/AndroApplication.java
@@ -26,18 +26,23 @@ import android.content.Context;
 import org.acra.*;
 import org.acra.annotation.*;
 
-@ReportsCrashes(formKey = "dG11eHBLM1BYSWY2VVVqeVFvdWpqQlE6MQ",
-        mode = ReportingInteractionMode.NOTIFICATION,
-        resNotifTickerText = R.string.crash_notif_ticker_text,
-        resNotifTitle = R.string.crash_notif_title,
-        resNotifText = R.string.crash_notif_text,
-        resNotifIcon = android.R.drawable.stat_notify_error,
-        resDialogText = R.string.crash_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_info,
-        resDialogTitle = R.string.crash_dialog_title,
-        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
-        resDialogOkToast = R.string.crash_dialog_ok_toast
-) 
+@ReportsCrashes(
+    formUri = "http://acra.geiger-online.ch/acra.php",
+    formUriBasicAuthLogin = "androvdr", // optional
+    formUriBasicAuthPassword = "69BoQNi3gEagw",  // optional
+    reportType = org.acra.sender.HttpSender.Type.JSON,
+    sendReportsAtShutdown = false
+    mode = ReportingInteractionMode.NOTIFICATION,
+    resNotifTickerText = R.string.crash_notif_ticker_text,
+    resNotifTitle = R.string.crash_notif_title,
+    resNotifText = R.string.crash_notif_text,
+    resNotifIcon = android.R.drawable.stat_notify_error,
+    resDialogText = R.string.crash_dialog_text,
+    resDialogIcon = android.R.drawable.ic_dialog_info,
+    resDialogTitle = R.string.crash_dialog_title,
+    resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
+    resDialogOkToast = R.string.crash_dialog_ok_toast
+)
 public class AndroApplication extends Application {
 
 	private static Context sContext;
